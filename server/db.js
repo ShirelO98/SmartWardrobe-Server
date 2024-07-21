eb4f41323b577e91ff537eece3d1e5236dcfcd4f
AdarAfek <adar257@gmail.com>
2024-07-21 13:16:49 +0300

    modify the export module

--- server/db.js
+++ server/db.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql2/promise");
 let connection;
-module.exports = {
-  async createConnection() {
-    connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
-  },
-  async initialize() {
-    if (connection) {
-      await connection.execute(`
+
+const createConnection = async () => {
+  connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+};
+const initialize = async () => {
+  if (connection) {
+    await connection.execute(`
         CREATE TABLE IF NOT EXISTS tbl_101_users (
           user_id INT AUTO_INCREMENT PRIMARY KEY,
           username VARCHAR(50) NOT NULL,
@@ -19,7 +19,7 @@ module.exports = {
           
         );
       `);
-      await connection.execute(`
+    await connection.execute(`
         CREATE TABLE IF NOT EXISTS tbl_101_wardrobes (
           wardrobe_id INT AUTO_INCREMENT PRIMARY KEY,
           wardrobe_name VARCHAR(100) NOT NULL,
@@ -27,12 +27,17 @@ module.exports = {
           FOREIGN KEY (user_id) REFERENCES users(user_id)
         );
       `);
-    }
-  },
-  
-  async closeConnection() {
-    if (connection) {
-      await connection.end();
-    }
-  },
+
+    await connection.execute();
+  }
 };
+
+const closeConnection = async () => {
+  if (connection) {
+    await connection.end();
+  }
+};
+
+module.exports= {
+  closeConnection,initialize,createConnection,
+}
