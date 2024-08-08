b21f623b882dc59d40b324722f2f27f808cc1962
ShirelO98 <shirelo983@gmail.com>
2024-08-08 13:27:41 +0300

    add wordrobe code

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ const { query } = require("../db");
 
 async function loginUser(req, res) {
   let rows2;
+  let wardrobecode
   const { username, password } = req.body;
   if (!username || !password) {
     return res.status(400).json({ error: "Missing Field" });
@@ -20,6 +21,12 @@ async function loginUser(req, res) {
         "SELECT * from tbl_101_stylists WHERE client_id = ?",
         [rows[0].id]);
     }
+    if (rows[0].user_type === 1) {
+      wardrobecode = await query(
+        "SELECT wardrobe_code from tbl_101_looks WHERE look_id = ?",
+        [rows[0].id]);
+    }
+
     res.json({
       UserID: rows[0].id,
       user_type: rows[0].user_type,
@@ -28,6 +35,7 @@ async function loginUser(req, res) {
       userImgUrl: rows[0].profile_image_url,
       selectedLook: rows2[0].select_look,
       messegeFromStylist: rows2[0].msg_stylist_to_client,
+      wardrobeCode: wardrobecode[0].wardrobe_code,
       msg: "Login successful",
     });
   } catch (err) {
