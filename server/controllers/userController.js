50b0637c1dec23996fa59b61496ab21a3a49d26e
ShirelO98 <shirelo983@gmail.com>
2024-08-08 13:05:12 +0300

    Adding select look

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const { query } = require("../db");
 
 async function loginUser(req, res) {
+  let rows2;
   const { username, password } = req.body;
   if (!username || !password) {
     return res.status(400).json({ error: "Missing Field" });
@@ -14,12 +15,18 @@ async function loginUser(req, res) {
     if (rows[0] === undefined) {
       return res.status(401).json({ error: "Wrong User Name or Password" });
     }
+    if (rows[0].user_type === 1) {
+      rows2 = await query(
+        "SELECT * from tbl_101_stylists WHERE client_id = ?",
+        [rows[0].id]);
+    }
     res.json({
       UserID: rows[0].id,
       user_type: rows[0].user_type,
       userFirstName: rows[0].f_name,
       userLastName: rows[0].l_name,
       userImgUrl: rows[0].profile_image_url,
+      selectedLook: rows2[0].select_look,
       msg: "Login successful",
     });
   } catch (err) {
